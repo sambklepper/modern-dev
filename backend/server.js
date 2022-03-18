2080ed59e511e28d522f33b0b78e7fa6c073a6f6
sambklepper <sambklepper@gmail.com>
2022-03-18 15:43:13 -0500

    MongoDB config.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require('express')
-const app = express()
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 8000
 
-const PORT = process.env.PORT || 5000
+// Connect to database
+connectDB()
+
+const app = express()
 
 app.get('/', (req, res) => {
   res.send('API running...')
 })
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`.magenta))
