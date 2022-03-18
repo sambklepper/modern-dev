89b582bee7554f663f461917ba147d756d77919e
sambklepper <sambklepper@gmail.com>
2022-03-18 16:23:31 -0500

    Error handler middleware.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 8000
 
@@ -15,5 +16,7 @@ app.use(express.urlencoded({ extended: false }))
 app.get('/', (req, res) => {
   res.send('API running...')
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`.magenta))
